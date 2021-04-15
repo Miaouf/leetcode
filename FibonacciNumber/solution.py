0410de508265590b5d7f8214b321be3bc3cf2423
Tanguy PEMEJA <74564644+tpemeja@users.noreply.github.com>
2021-04-15 09:43:36 +0200

    made by Tanguy PEMEJA

---
+++ FibonacciNumber/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0 : return 0
+        
+        L = [0]*(n+1)
+        L[1]=1
+        
+        for i in range(2,n+1):
+            L[i]=L[i-1]+L[i-2]
+        return L[n]
