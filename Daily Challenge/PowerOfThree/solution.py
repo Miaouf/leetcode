13c209d7de0ff2cceb9cbd44d06beb0a7be50b1f
Tanguy PEMEJA <74564644+tpemeja@users.noreply.github.com>
2021-04-27 09:30:27 +0200

    made by Tanguy PEMEJA

--- Daily Challenge/PowerOfThree/solution.py
+++ Daily Challenge/PowerOfThree/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        #1162261467 == 3**19, 3**20 > 2**31 - 1
+        #1162261467 = 3**19, 3**20 > 2**31 - 1
         return (n > 0) and (1162261467 % n) == 0 
