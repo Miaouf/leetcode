f4dadc09a0395014675a2bb7782b1da56f07e108
Tanguy PEMEJA <74564644+tpemeja@users.noreply.github.com>
2021-04-28 09:30:06 +0200

    made by Tanguy PEMEJA

--- Daily Challenge/UniquePathsII/solution.py
+++ Daily Challenge/UniquePathsII/solution.py
@@ -1,10 +1,14 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-       
+        if(obstacleGrid[0][0] == 1):
+            return 0
+        else:
+            obstacleGrid[0][0] = 1
+            
         for i in range(len(obstacleGrid)):
             for j in range(len(obstacleGrid[0])):
                 if(i==0 and j ==0):
-                    obstacleGrid[i][j] = (obstacleGrid[i][j]+1)%2
+                    pass
                     
                 elif obstacleGrid[i][j] != 1:
                     obstacleGrid[i][j] = (obstacleGrid[i-1][j] if i != 0 else 0)\
