b7218c226d8e1a27b5eaf4c2288c9edc186e16c1
Tanguy PEMEJA <74564644+tpemeja@users.noreply.github.com>
2021-05-11 17:31:04 +0200

    made by Tanguy PEMEJA

---
+++ Daily Challenge/MaximumPointsYouCanObtainFromCards/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        max_score = sum(cardPoints[:k])
+        score = max_score
+        start = k
+        end = 0
+        
+        for i in range(k):
+            start -= 1
+            end -= 1
+            score += (cardPoints[end] - cardPoints[start])
+            max_score = max(max_score,score)
+            
+        return max_score    
