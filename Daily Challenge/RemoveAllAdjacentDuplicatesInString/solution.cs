91fd271033da50c9b411df9c74c09f1bec8221cc
Maxime Rosay <47741889+Miaouf@users.noreply.github.com>
2021-06-30 10:46:40 +0200

    Update solution.cs

--- Daily Challenge/RemoveAllAdjacentDuplicatesInString/solution.cs
+++ Daily Challenge/RemoveAllAdjacentDuplicatesInString/solution.cs
@@ -19,4 +19,27 @@ public class Solution {
         }
         return res;
     }
+    public string RemoveDuplicatesWithStack(string s) {
+        
+        var stack = new Stack<char>();
+        var chars = s.ToCharArray();
+
+        foreach (var c in chars) {
+            if (stack.Count == 0) {
+                stack.Push(c);
+                continue;
+            }
+
+            if (stack.Peek() == c)
+                stack.Pop();
+            else
+                stack.Push(c);
+        }
+
+        var result = new char[stack.Count];
+        for (var i = stack.Count - 1; i >= 0; i--)
+            result[i] = stack.Pop();
+
+        return new string(result);
+    }
 }
